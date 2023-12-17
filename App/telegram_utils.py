bb63a1f255d970aca9781b4b9b0efd998701f1e6
ngdkhanh11 <100998717+ngdkhanh11@users.noreply.github.com>
2023-12-17 23:05:19 +0700

    Add files via upload

--- App/telegram_utils.py
+++ App/telegram_utils.py
@@ -1,8 +1,9 @@
+import asyncio
 import telegram
 
 # token account telegram
 my_token = "6768843655:AAGL_HDuThL7UgouNQ1fXsefZYSs3O-4uGI"
-my_chatid = '5397689542'
+my_chatid = '-1001996069923'
 
 # create Bot
 bot = telegram.Bot(token=my_token)
@@ -10,12 +11,12 @@ bot = telegram.Bot(token=my_token)
 # send text message
 
 
-def send_telegram(photo_path="data/alert.png"):
+async def send_telegram(photo_path="./data/alert.png"):
     try:
-        bot = telegram.Bot(token=my_token)
-        bot.sendPhoto(chat_id=my_chatid, photo=open(
+        await bot.sendPhoto(chat_id=my_chatid, photo=open(
             photo_path, "rb"), caption="Có người đang đứng ở quầy hàng!!!")
+        print("Send sucess")
     except Exception as ex:
         print("Can not send message telegram ", ex)
 
-    print("Send sucess")
+asyncio.run(send_telegram())
